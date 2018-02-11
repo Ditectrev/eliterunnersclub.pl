fb1cf19d939c00e8773ceb2d68d50bea4b070205
Daniel Danielecki <danieldanielecki@DMBP.local>
2018-02-11 19:33:49 +0100

    add intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { IntroComponent } from './intro/intro.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule
