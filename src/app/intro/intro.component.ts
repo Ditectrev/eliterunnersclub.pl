d62ae9264570bc7bb7aa33462bcf742031896cb2
Daniel Danielecki <daniel.danielecki@mail.com>
2018-10-15 10:17:16 +0300

    Edit: slider

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./intro.component.scss']
 })
 export class IntroComponent {
-
+  slides = [
+    {
+      image: 'assets/images/intro-image1.jpg',
+      title: 'Bieganie to wspaniały świat'
+    },
+    {
+      image: 'assets/images/intro-image2.jpg',
+      title: 'Bieganie to wspaniały świat'
+    },
+    {
+      image: 'assets/images/intro-image3.jpg',
+      title: 'Bieganie to wspaniały świat'
+    }
+  ]
 }
